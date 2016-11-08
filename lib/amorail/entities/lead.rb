028dcb44c0ed71e94477bbf7327c37ce939ba2ff
Surgeon <Surgeon1989@gmail.com>
2016-11-08 17:30:10 +0300

    Update lead.rb

--- lib/amorail/entities/lead.rb
+++ lib/amorail/entities/lead.rb
@@ -3,7 +3,7 @@ module Amorail
   class Lead < Amorail::Entity
     amo_names "leads"
 
-    amo_field :name, :price, :status_id, :tags
+    amo_field :name, :price, :status_id, :tags, :custom_fields
 
     validates :name, :status_id, presence: true
 
