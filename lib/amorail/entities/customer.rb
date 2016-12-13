4bac6273f3651786deb30c4dad6e9f5ed254f5db
Surgeon <Surgeon1989@gmail.com>
2016-12-13 17:22:19 +0300

    Create customer.rb

---
+++ lib/amorail/entities/customer.rb
@@ -0,0 +1,25 @@
+module Amorail
+  # AmoCRM lead entity
+  class Customer < Amorail::Entity
+    amo_names "customers"
+
+    amo_field :name, :tags, :custom_fields, :contact_id
+
+    # validates :name, :status_id, presence: true
+
+    def reload
+      @contacts = nil
+      super
+    end
+
+    # Return list of associated contacts
+    def contacts
+      fail NotPersisted if id.nil?
+      @contacts ||=
+          begin
+            links = Amorail::ContactLink.find_by_leads(id)
+            links.empty? ? [] : Amorail::Contact.find_all(links.map(&:contact_id))
+          end
+    end
+  end
+end
